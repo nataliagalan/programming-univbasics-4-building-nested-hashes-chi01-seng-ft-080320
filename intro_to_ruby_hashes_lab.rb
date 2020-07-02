8dd56511bb6b56291995c9eef5ec0bcfc16f2c84
Natalia Galan <n8galan@gmail.com>
2020-07-02 21:16:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  # return a hash with one key called :railroads that points to an empty hash
+  hash { :railroads => {} }
 
 end
 
