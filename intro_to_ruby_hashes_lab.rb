a4e736bf54d525d9ce9d5f0d65bd103809061107
Natalia Galan <n8galan@gmail.com>
2020-07-02 21:17:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   # return a hash with one key called :railroads that points to an empty hash
   hash { :railroads => {} }
-
+binding.pry
 end
 
 def monopoly_with_second_tier
